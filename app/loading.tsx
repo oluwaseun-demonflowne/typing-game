8557747c6a17e36b1c54d93611fd8c25dd729ea6
demonflowne <ibuemmanuel60@gmail.com>
2024-02-03 03:01:17 +0100

    Added Suspense loader

---
+++ app/loading.tsx
@@ -0,0 +1,9 @@
+export default function Loading() {
+  return (
+    <div className="w-[100%] h-[90vh] flex justify-center items-center">
+        <svg className="animate-spin h-5 w-5 mr-3 ..." viewBox="0 0 24 24"></svg>;
+    </div>
+  );
+}
+
+
